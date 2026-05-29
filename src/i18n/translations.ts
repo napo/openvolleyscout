ad4f71e32af46f2a1974d193574768801c0c1ba2
Maurizio Napolitano <napo@fbk.eu>
2026-05-29 18:23:32 +0200

    chinese language

--- src/i18n/translations.ts
+++ src/i18n/translations.ts
@@ -3,12 +3,14 @@ import { en } from './locales/en';
 import { it } from './locales/it';
 import { de } from './locales/de';
 import { sl } from './locales/sl';
+import { zh } from './locales/zh';
 
 export const translations = {
   it,
   en,
   de,
   sl,
+  zh,
 } as const;
 
 export type TranslationKey = keyof typeof it;
